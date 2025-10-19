c3b909274c870b16fd7b012bd7e0f286c629e06c
Michael <michaeljedrz@gmail.com>
2025-10-19 22:30:18 +0200

    chore: fonts

--- frontend/src/components/layout/Main.jsx
+++ frontend/src/components/layout/Main.jsx
@@ -8,7 +8,7 @@ export default function Main() {
       <header className="w-full">
         <MobileHeader />
         <div className="flex w-full px-7 md:py-6 py-5 flex-col gap-y-5 items-center md:flex-row md:justify-between md:items-center md:border-b md:border-b-light-grey/20">
-          <h1 className="text-2xl">My Applications</h1>
+          <h2 className="text-2xl">My Applications</h2>
           <div className="flex items-center gap-x-5">
             <Button icon={<PlusIcon />} title="Add new" />
             <Button icon={<DownloadIcon />} title="Download" />
@@ -16,7 +16,7 @@ export default function Main() {
         </div>
       </header>
       <main className="h-dvh flex flex-col justify-center items-center overscroll-contain">
-        <h2>bewerbungen sind hier</h2>
+        <h2 className="text-3xl">bewerbungen sind hier</h2>
       </main>
     </div>
   );
