616f6f8268432956b12ced60de83aa0a5f2aff39
Michael <michaeljedrz@gmail.com>
2025-10-26 19:27:32 +0100

    add first job modal style component

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import Button from "../ui/Button";
 import { DownloadIcon, PlusIcon } from "../ui/Icons";
+import JobForm from "../ui/JobForm";
 import MobileHeader from "./MobileHeader";
 
 export default function Header() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <header className="w-full">
       <MobileHeader />
       <div className="flex w-full px-7 md:py-6 py-5 flex-col gap-y-5 items-center md:flex-row md:justify-between md:items-center md:border-b md:border-b-light-grey/20">
-        <h2 className="text-2xl flex gap-x-1.5">
-          <span>Welcome Back</span>
-          <span className="font-extrabold text-accent">Max</span>
-        </h2>
+        <h2 className="text-2xl flex gap-x-1.5">Welcome Back</h2>
         <div className="flex items-center gap-x-5">
-          <Button icon={<PlusIcon />} title="Add new" />
+          <Button
+            onClick={() => setShowModal(true)}
+            icon={<PlusIcon />}
+            title="Add new"
+          />
           <Button icon={<DownloadIcon />} title="Download" />
         </div>
       </div>
+      {showModal &&
+        createPortal(
+          <JobForm onClose={() => setShowModal(false)} />,
+          document.body,
+        )}
     </header>
   );
 }
