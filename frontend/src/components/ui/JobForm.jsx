09cd6721d5ce3c811b57d8c8021fe5deb98e4174
Michael <michaeljedrz@gmail.com>
2025-10-27 15:59:51 +0100

    cleanup form a bit

--- frontend/src/components/ui/JobForm.jsx
+++ frontend/src/components/ui/JobForm.jsx
@@ -1,50 +1,59 @@
 import { CloseIcon } from "./Icons";
 
+function saveAction() {
+  console.log(form);
+}
+
 export default function JobForm({ onClose }) {
-  const inputStyles = `border border-dark-secondary rounded-lg px-3 py-2`;
+  const inputStyles = `border border-dark-secondary rounded-lg px-3 py-2 resize-none`;
+  const labelStyles = `flex flex-col gap-y-2 text-light-grey`;
   return (
     <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-50">
       <div className="bg-dark-primary rounded-xl border border-dark-secondary p-6 w-[393px]">
-        <div className="w-full flex justify-end">
+        <div className="w-full flex justify-end items-center mb-2">
           <CloseIcon onClick={onClose} className="cursor-pointer" />
         </div>
-        <form className="flex flex-col gap-y-5">
-          <div className="flex flex-col gap-y-2 text-light-grey">
-            <label htmlFor="company">Company</label>
+        <form action={saveAction} className="flex flex-col gap-y-5">
+          <label className={labelStyles}>
+            Company
             <input
               type="text"
               name="company"
-              id="company"
               autoComplete="on"
+              required
               className={inputStyles}
             />
-          </div>
-          <div className="flex flex-col gap-y-2 text-light-grey">
-            <label htmlFor="job">Job</label>
-            <input type="text" name="job" id="job" className={inputStyles} />
-          </div>
-          <div className="flex flex-col gap-y-2 text-light-grey">
-            <label htmlFor="date">Date</label>
-            <input type="date" name="date" id="date" className={inputStyles} />
-          </div>
-          <div className="flex flex-col gap-y-2 text-light-grey">
-            <label htmlFor="location">Location</label>
+          </label>
+          <label className={labelStyles}>
+            Job
+            <input type="text" name="job" required className={inputStyles} />
+          </label>
+          <label className={labelStyles}>
+            Date
+            <input type="date" name="date" required className={inputStyles} />
+          </label>
+          <label className={labelStyles}>
+            Location
             <input
               type="text"
               name="location"
-              id="location"
+              required
               className={inputStyles}
             />
-          </div>
-          <div className="flex flex-col gap-y-2 text-light-grey">
-            <label htmlFor="status">Status</label>
-            <select name="status" id="status" className={inputStyles}>
+          </label>
+          <label className={labelStyles}>
+            Status
+            <select name="status" required className={inputStyles}>
               <option value="default">Choose Status</option>
               <option>Offer</option>
               <option>Pending</option>
               <option>Rejected</option>
             </select>
-          </div>
+          </label>
+          <label className={labelStyles}>
+            Notes
+            <textarea name="notes" rows="5" className={inputStyles}></textarea>
+          </label>
           <div className="flex gap-x-2">
             <button
               type="submit"
