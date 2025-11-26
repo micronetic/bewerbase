b87ffaeef5bdd97ae0533de70b3b8d6f572d4bcb
Michael <michaeljedrz@gmail.com>
2025-11-26 22:58:53 +0100

    fix: application info inner divs

--- frontend/src/app/components/ui/ApplicationInfo.jsx
+++ frontend/src/app/components/ui/ApplicationInfo.jsx
@@ -10,23 +10,27 @@ export default function ApplicationInfo({ onClose, data }) {
           className="flex flex-col items-center gap-y-2 bg-dark-primary rounded-xl border border-dark-secondary p-6 w-[375px]"
         >
           <CloseIcon onClick={onClose} className="self-end cursor-pointer" />
-          <h2 className="text-white text-4xl font-bold capitalize">
-            {data.company}
-          </h2>
-          <p>{data.job}</p>
-          <p className="text-light-grey">
-            Applied on {new Date(data.date).toLocaleDateString("de-DE")}
-          </p>
-          <div className="flex items-center gap-x-1.5">
-            <PinIcon className="w-5 h-auto" />
-            <p className="text-light-grey text-sm capitalize">
-              {data.location}
-            </p>
-          </div>
-          <Label labelTitle={data.status} />
-          <div className=" w-full flex flex-col gap-y-4">
-            <h3 className="text-lg">Notes</h3>
-            <p>{data.notes}</p>
+          <div className="flex flex-col gap-y-9">
+            <div className="flex flex-col items-center gap-y-1.5">
+              <h2 className="text-white text-4xl font-bold capitalize">
+                {data.company}
+              </h2>
+              <p>{data.job}</p>
+              <p className="text-light-grey">
+                Applied on {new Date(data.date).toLocaleDateString("de-DE")}
+              </p>
+              <div className="flex justify-center items-center gap-x-1.5">
+                <PinIcon className="w-5 h-auto" />
+                <p className="text-light-grey text-sm capitalize">
+                  {data.location}
+                </p>
+              </div>
+              <Label labelTitle={data.status} />
+            </div>
+            <div className=" w-full flex flex-col gap-y-2">
+              <h3 className="text-lg font-bold">Notes</h3>
+              <p>{data.notes}</p>
+            </div>
           </div>
         </div>
       </div>
