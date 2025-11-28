4035e8fe46af5831f044c4e4739fb9c005dcb720
Michael <michaeljedrz@gmail.com>
2025-11-28 13:38:21 +0100

    fix: route

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -2,16 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import App from "../app/App.jsx";
 import Home from "../app/pages/Home.jsx";
 import Jobs from "../app/pages/Jobs.jsx";
-import Section from "../landing/components/layout/Section.jsx";
 import LandingPage from "../landing/LandingPage.jsx";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />}>
-          <Route index element={<Section />} />
-        </Route>
+        <Route path="/" element={<LandingPage />} />
 
         <Route path="/app" element={<App />}>
           <Route index element={<Home />} />
