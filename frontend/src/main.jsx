8d012a0c45bc305625d89b9548d6fed935fd89f8
Michael <michaeljedrz@gmail.com>
2026-01-25 19:04:09 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/bewerbase">
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
